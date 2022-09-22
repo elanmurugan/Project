9454e3305a5691c438238686478aefc41526a9eb
elanmurugan <elanrc1998@gmail.com>
2022-09-22 17:24:05 +0530

    javaproject

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,35 @@
+package org.sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		List<Integer> a= new ArrayList<Integer>();
+		a.add(10);
+		a.add(1);
+		a.add(20);
+		
+		for (int i = 0; i < a.size(); i++) {
+			
+			if ((i%2)==0) {
+				
+				System.out.println("even number");
+				
+			}
+			else {
+				
+				System.out.println("odd number");
+
+			}
+			
+		}
+		
+		
+		
+		
+	}
+
+}
